a5ab3757f7187398625418341f0273cd691cfb54
hientominh <tominhhien97@gmail.com>
2018-05-23 20:06:07 +0700

    new update

--- heuristic-n-queens.cpp
+++ heuristic-n-queens.cpp
@@ -10,25 +10,21 @@ pair<vector<int>,int> data;
 int n;
 double T;
 
-int C(int n) {
-    if(n < 2){
-        return 0;
-    }
-    if(n == 2) {
-        return 1;
-    }
-    return (n - 1) * n / 2;
-}
-
 void init() {
     cout << "Input n = ";
     cin >> n;
     T = n;
     srand(time(0));
+    vector<int> a;
     for(int i = 0; i < n; i++) {
-        data.first.push_back(i);
+        a.push_back(i);
+        //data.first.push_back(i);
     }
-    random_shuffle(data.first.begin(), data.first.end());
+    for(int i = 0; i < n; i++) {
+        int temp = random() % a.size();
+        data.first.push_back(a[temp]);
+        a.erase(a.begin() + temp);
+    }
     int result = 0;
     for(int i = 0 ; i < n - 1; i++) {
         for(int j = i + 1 ; j < n; j++){
@@ -40,49 +36,40 @@ void init() {
     data.second = result;
 }
 
-int heuristic(pair<vector<int>,int> data) {
-    int result = 0;
-    int n = data.first.size();
-    int *temp1, * temp2;
-    temp1 = new int[n];
-    temp2 = new int[n];
-    for(int i = 0; i < n; i++) {
-        temp1[i] = data.first[i] + i;
-        temp2[i] = data.first[i] - i;
-    }
-    sort(temp1, temp1 + n);
-    sort(temp2, temp2 + n);
-    int i = 0;
-    while(i < n-1) {
-        int j = i + 1;
-        int count = 1;
-        while(temp1[i] == temp1[j]) {
-            j++;
-            count++;
+pair<vector<int>,int> Next_move(pair<vector<int>,int> data, int i, int j) {
+    pair<vector<int>,int> temp = data;
+    int xungDot = data.second;
+    int jj = data.first[i];
+    int ii = data.first[j];
+    temp.first[i] = ii;
+    temp.first[j] = jj;
+    int dem1 = 0, dem2 = 0;
+
+    for (int k = 0; k < data.first.size(); k++) {
+        if (k != i) {
+            if (ii + i == temp.first[k] + k
+                || ii - i == temp.first[k] - k) {
+                dem1++;
+            }
+
+            if (jj + i == data.first[k] + k
+                || jj - i == data.first[k] - k) {
+                dem2++;
+            }
         }
-        result += C(count);
-        i = j;
-        if(j >= n) {
-            break;
-        }
-    }
-    i = 0;
-    while(i < n-1) {
-        int j = i + 1;
-        int count = 1;
-        while (temp2[i] == temp2[j]) {
-            j++;
-            count++;
-        }
-        result += C(count);
-        i = j;
-        if (j >= n) {
-            break;
+        if (k != j) {
+            if (jj + j == temp.first[k] + k
+                || jj - j == temp.first[k] - k) {
+                dem1++;
+            }
+            if (ii + j == data.first[k] + k
+                || ii - j == data.first[k] - k) {
+                dem2++;
+            }
         }
     }
-    delete(temp1);
-    delete(temp2);
-    return result;
+    temp.second = (xungDot - dem2 + dem1);
+    return temp;
 }
 
 void show(pair<vector<int>,int> data) {
@@ -92,22 +79,19 @@ void show(pair<vector<int>,int> data) {
 }
 
 void solve() {
-    bool stop = false;
     srand(time(0));
     double p;
     int step = 0;
     while(T > 0) {
         if(data.second == 0){
-            stop = true;
             show(data);
             break;
         }
         T *= 0.99;
-        pair<vector<int>, int> next_move = data;
-        swap(next_move.first[rand()%n], next_move.first[random()%n]);
-        next_move.second = heuristic(next_move);
+        int i = rand()%n;
+        int j = rand()%n;
+        pair<vector<int>, int> next_move = Next_move(data,i,j);
         if(next_move.second == 0) {
-            stop = true;
             cout << next_move.second << endl;
             cout << "Steps: " << step << endl;
             cout << "Result: ";
@@ -128,9 +112,6 @@ void solve() {
                 cout << next_move.second << endl;
             }
         }
-    }
-    if(stop != true) {
-        cout << "Failed!";
     }
 }
 
